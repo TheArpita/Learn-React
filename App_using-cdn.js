6a25a3256195422955c8b8cc5fd79a66d4264128
TheArpita <arpitakarmakar5027@gmail.com>
2023-01-17 16:15:45 +0530

    removing unwanted folders from git

--- App_using-cdn.js
+++ App_using-cdn.js
@@ -1,7 +1,4 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-
-const heading = React.createElement("h1", { id: "title"}, "Hello React");
+const heading = React.createElement("h1", { id: "title"}, "Hello React"); //html-element, props, children
 const heading1 = React.createElement("h1", { id: "title1"}, "Hello React 1");
 const container = React.createElement("div", { id: "container" }, [heading, heading1]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
